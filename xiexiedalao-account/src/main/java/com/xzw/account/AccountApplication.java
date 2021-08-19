d0bdd03015f6543028cb4dedca5d653d29315147
谢谢大佬 <luck_xzw@163.com>
2021-08-19 21:52:34 +0800

    first commit

---
+++ xiexiedalao-account/src/main/java/com/xzw/account/AccountApplication.java
@@ -0,0 +1,16 @@
+package com.xzw.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xieziwei
+ * @version 2021/8/19
+ * @description
+ */
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class,args);
+    }
+}
